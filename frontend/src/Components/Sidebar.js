972cf3e342bf304bb640c4cf825801b3e9a7f405
A.Mokhachane <amandamokhachane88@gmail.com>
2025-07-15 11:27:49 +0200

    Sidebar complete

--- frontend/src/Components/Sidebar.js
+++ frontend/src/Components/Sidebar.js
@@ -1,18 +1,54 @@
 import React from 'react';
 import './Sidebar.css';
 import { NavLink } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import profile from './Images/profile.jpg';
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h2 className="sidebar-title">Menu</h2>
+      <h2 className="sidebar-title">Nerdy</h2>
+
+       <div className="sidebar-search">
+          <div className="search-input-wrapper">
+            <i className="fas fa-search search-icon"></i>
+            <input type="text" placeholder="Search..." />
+          </div>
+       </div>
+
       <nav className="sidebar-nav">
-        <NavLink to="/" className="sidebar-link" activeclassname="active">Dashboard</NavLink>
-        <NavLink to="/clients" className="sidebar-link" activeclassname="active">Clients</NavLink>
-        <NavLink to="/timesheet" className="sidebar-link" activeclassname="active">Timesheet</NavLink>
-        <NavLink to="/register" className="sidebar-link" activeclassname="active">Register</NavLink>
-        <NavLink to="/login" className="sidebar-link" activeclassname="active">Login</NavLink>
+        <NavLink to="/" className="sidebar-link" activeclassname="active">
+          <i className="fas fa-home icon-left"></i> Dashboard
+        </NavLink>
+        <NavLink to="/clients" className="sidebar-link" activeclassname="active">
+          <i className="fas fa-users icon-left"></i> Clients
+        </NavLink>
+        <NavLink to="/timesheet" className="sidebar-link" activeclassname="active">
+          <i className="fas fa-clock icon-left"></i> Timesheet
+        </NavLink>
+        <NavLink to="/register" className="sidebar-link" activeclassname="active">
+          <i className="fas fa-user-plus icon-left"></i> Register
+        </NavLink>
+        <NavLink to="/login" className="sidebar-link" activeclassname="active">
+          <i className="fas fa-sign-in-alt icon-left"></i> Login
+        </NavLink>
       </nav>
+
+      <div className="sidebar-profile-wrapper">
+        <div className="sidebar-profile">
+            <img
+              src={profile}
+              alt="Profile"
+              className="profile-pic"
+            />
+          <div className="profile-info">
+            <span className="profile-name">Amanda Mokhachane</span>
+            <button className="profile-options-btn">
+              <i className="fas fa-ellipsis-v"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
